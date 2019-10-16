2a769a413a80c4e96c1ddf18d308df661454efa1
Piotr <piotr43a@gmail.com>
2019-10-16 12:48:06 +0200

    create rest mappings

--- src/main/kotlin/com/piotr/tictactoe/rest/GameController.kt
+++ src/main/kotlin/com/piotr/tictactoe/rest/GameController.kt
@@ -1,8 +1,11 @@
 package com.piotr.tictactoe.rest
 
 import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("api/game/")
@@ -13,8 +16,28 @@ class GameController {
     return "Works"
   }
 
-  @PostMapping("createGame")
+  @PostMapping("queue")
+  fun joinToQueue() {
+
+  }
+
+  @DeleteMapping("queue")
+  fun removeFromQueue() {
+
+  }
+
+  @PostMapping("create")
   fun createGame() {
+
+  }
+
+  @PutMapping("{gameId}/field")
+  fun setField(@PathVariable("gameId") gameId: String) {
+
+  }
+
+  @PutMapping("{gameId}/reset")
+  fun resetGame(@PathVariable("gameId") gameId: String) {
 
   }
 
