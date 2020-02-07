4a7dd58bef8f7a99a43fccb05be857cd3974cedd
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-02-07 21:45:54 +0100

    check if field is already taken

--- src/main/kotlin/com/piotr/tictactoe/move/domain/MoveFacade.kt
+++ src/main/kotlin/com/piotr/tictactoe/move/domain/MoveFacade.kt
@@ -15,7 +15,8 @@ class MoveFacade {
   private lateinit var moveRepository: MoveRepository
 
   fun setMove(gameId: Long, fieldIndex: Int, mark: FieldMark): MoveDto {
-    checkFieldIndex(fieldIndex)
+    checkFieldIndexRange(fieldIndex)
+    checkIfFieldIndexIsEmpty(gameId, fieldIndex)
     return Move(
         gameId = gameId,
         fieldIndex = fieldIndex,
@@ -28,15 +29,22 @@ class MoveFacade {
 
   fun getAllMoves(gameId: Long): List<MoveDto> = moveRepository.findMovesByGameId(gameId).map(Move::toDto)
 
-  private fun checkFieldIndex(fieldIndex: Int) {
+  private fun getNextCounter(gameId: Long): Int {
+    val lastMove = moveRepository.findFirstMoveByGameIdOrderByIdDesc(gameId)
+    return lastMove?.counter?.plus(1) ?: 0
+  }
+
+  private fun checkFieldIndexRange(fieldIndex: Int) {
     if (fieldIndex !in VALID_FIELDS_INDEXES) {
       throw InvalidFieldIndexRangeException("Field index should be in range $VALID_FIELDS_INDEXES")
     }
   }
 
-  private fun getNextCounter(gameId: Long): Int {
-    val lastMove = moveRepository.findFirstMoveByGameIdOrderByIdDesc(gameId)
-    return lastMove?.counter?.plus(1) ?: 0
+  private fun checkIfFieldIndexIsEmpty(gameId: Long, fieldIndex: Int) {
+    val moves = getAllMoves(gameId)
+    if (moves.any { it.fieldIndex == fieldIndex }) {
+      throw FieldAlreadyTakenException()
+    }
   }
 
   fun createMoveFacade() = MoveFacade()
