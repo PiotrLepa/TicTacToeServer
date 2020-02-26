c5f1ef87bdef16c5a883d9f5abd2da484228c915
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-02-26 17:05:13 +0100

    add default locale & pass locale via header instead of param

--- src/main/kotlin/com/piotr/tictactoe/core/locale/LocaleCreator.kt
+++ src/main/kotlin/com/piotr/tictactoe/core/locale/LocaleCreator.kt
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
-import org.springframework.web.servlet.i18n.SessionLocaleResolver
 import java.util.Locale
 
 @Configuration
@@ -14,25 +14,23 @@ class LocaleCreator {
 
   @Bean
   fun localeResolver(): LocaleResolver =
-      SessionLocaleResolver().apply {
-        setDefaultLocale(DEFAULT_LOCALE)
+      AcceptHeaderLocaleResolver().apply {
+        defaultLocale = DEFAULT_LOCALE
       }
 
   @Bean
-  fun localeChangeInterceptor(): LocaleChangeInterceptor =
-      LocaleChangeInterceptor().apply {
-        paramName = LOCALE_PARAM_NAME
-      }
+  fun localeChangeInterceptor(): LocaleChangeInterceptor = LocaleChangeInterceptor()
 
-  @Bean(name = ["messageSource"])
-  fun bundleMessageSource(): MessageSource = ResourceBundleMessageSource()
+  @Bean
+  fun messageSource(): MessageSource = ResourceBundleMessageSource()
       .apply {
-        setBasename("locale/messages")
+        setBasename("i18n/messages")
         setDefaultEncoding("UTF-8")
+        setFallbackToSystemLocale(false)
+        setDefaultLocale(DEFAULT_LOCALE)
       }
 
   companion object {
     private val DEFAULT_LOCALE = Locale.ENGLISH
-    private const val LOCALE_PARAM_NAME = "lang"
   }
 }
