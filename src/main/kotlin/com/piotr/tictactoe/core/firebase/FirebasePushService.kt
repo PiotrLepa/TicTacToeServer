c7c02f52e7ebb2195939a19458ea097dc5c237d6
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-04-26 16:38:41 +0200

    send fcm notification message instead of data message

--- src/main/kotlin/com/piotr/tictactoe/core/firebase/FirebasePushService.kt
+++ src/main/kotlin/com/piotr/tictactoe/core/firebase/FirebasePushService.kt
@@ -2,11 +2,12 @@ package com.piotr.tictactoe.core.firebase
 
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.Notification
+import com.piotr.tictactoe.core.extensions.getLocalizedMessage
 import com.piotr.tictactoe.multiplayerGame.dto.MultiplayerGameCreatedDto
 import com.piotr.tictactoe.user.dto.UserDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.MessageSource
-import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.stereotype.Service
 
 @Service
@@ -20,16 +21,28 @@ class FirebasePushService @Autowired constructor(
     gameDto: MultiplayerGameCreatedDto,
     opponent: UserDto
   ) {
+    val notification = Notification(
+        messageSource.getLocalizedMessage("push.game_request.title"),
+        messageSource.getLocalizedMessage("push.game_request.body", opponent.username)
+    )
     val message = Message.builder()
         .setToken(token)
+        .setNotification(notification)
         .putAllData(mapOf(
-            "title" to messageSource.getMessage("push.game_request.title", null, LocaleContextHolder.getLocale()),
-            "body" to messageSource.getMessage("push.game_request.body", arrayOf(opponent.username), LocaleContextHolder.getLocale()),
             "gameId" to gameDto.gameId.toString(),
             "yourMark" to gameDto.yourMark.toString(),
-            "playerType" to gameDto.playerType.toString()
+            "playerType" to gameDto.playerType.toString(),
+            getFlutterClickAction()
         ))
         .build()
     firebaseMessaging.send(message)
+  }
+
+  private fun getFlutterClickAction(): Pair<String, String> =
+      FLUTTER_CLICK_ACTION_KEY to FLUTTER_CLICK_ACTION_VALUE
+
+  companion object {
+    private const val FLUTTER_CLICK_ACTION_KEY = "click_action"
+    private const val FLUTTER_CLICK_ACTION_VALUE = "FLUTTER_NOTIFICATION_CLICK"
   }
 }
